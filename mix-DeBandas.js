14608f5ea6550b8d0a06b6772e5369fa9308600d
Ezequiel Gomez <131683918+Megametaldeth@users.noreply.github.com>
2023-05-23 12:53:22 -0300

    agrega orden de bandas a mix-DeBandas.js

--- mix-DeBandas.js
+++ mix-DeBandas.js
@@ -1,1 +1,7 @@
 console.log('Aca estan las bandas piolas');
+
+console.log(`
+1. Toca Pantera para abrir el show,
+2. Toca Symphony X para dejar a la gente re manija,
+3. Cierra Megadeth todo bien polenta.
+`);
