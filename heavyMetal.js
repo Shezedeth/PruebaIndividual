1c84c0f54f4bc69ec4fa20ffb80fa293729002ee
Shezedeth <sheze.gomez@gmail.com>
2023-05-23 16:42:15 -0300

    se agrego heavyMetal.js

---
+++ heavyMetal.js
@@ -0,0 +1,15 @@
+const metal = [
+    {
+        AlmaFuerte: `Heavy Metal`,
+        Integrantes: 3
+    },
+    {
+        Megadeth: `Thrash Metal`,
+        Integrantes: 4
+    },
+    {
+        DreamTheater: `Power Metal Progresivo`,
+        Integrantes: 5
+    }
+
+];
